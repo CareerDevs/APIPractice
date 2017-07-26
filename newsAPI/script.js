f127a11438236c24b6623483d0ab3a7629b8bf85
Cliff <tools@3csonline.com>
2017-07-26 00:17:58 +0000

    add 2nd api call to display titles

--- newsAPI/script.js
+++ newsAPI/script.js
@@ -10,15 +10,36 @@ $(document).ready(function() {
             // console.log(response.sources[2]);
             var sources = response.sources;
             // console.log(sources[2]);
-            var html = "<select class='form-control' id='source'>";
+            var html = "<select class='form-control' id='sourceOption'>";
             $.each(sources, function(index, source){
                 // console.log(source);
-                html += "<option>" + source.name + "</option>"
+                html += "<option value='" + source.id + "'>" + source.name + "</option>"
             })
             html += "</select>";
-            console.log(html)
+            // console.log(html)
             $(".form-group").html(html)
-            
         }
+    })
+    
+    $("#source").submit(function(event) {
+        event.preventDefault();
+        var id = $('#sourceOption').val();
+        var url = "https://newsapi.org/v1/articles";
+        var data = {apiKey:"c1156996764f428a8a37cb419fc8a372", source: id};
+        $.ajax({
+            url: url,
+            data: data,
+            type: "GET",
+            success: function(response) {
+                // console.log(response);
+                var articles = response.articles;
+                var html = "<ul class='list-group'>";
+                $.each(articles, function(i, article) {
+                    html += "<li class='list-item'>" + article.title + "</li>";
+                })
+                html += "</ul>";
+                $("#articles").html(html);
+            }
+        })
     })
 })
